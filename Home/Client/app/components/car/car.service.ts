f90a9154c812ca6570e286b3fbfb40a888be741f
Alec Malcolm <alec.malcolm@camis.com>
2018-07-24 21:19:49 -0400

    fixed the stored procedure for GetCar

--- Home/Client/app/components/car/car.service.ts
+++ Home/Client/app/components/car/car.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpParams, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/do';
 
 import { Car } from './car';
+import { RequestOptions } from '../../../../node_modules/@angular/http';
 
 @Injectable()
 export class CarService {
@@ -19,10 +20,12 @@ export class CarService {
     }
 
     getCar(id: number): Observable<Car> {
+        let headers = new HttpHeaders({ 'Content-Type': 'applciation/json' });
         let params = new HttpParams()
-            .set('id', String(id));
-        let requestOptions = { params: params };
-        return this._http.get(this._carUrl, requestOptions)
+            .set('id', id.toString());
+
+        let requestOptions = { headers: headers, params: params };
+        return this._http.get<Car>(this._carUrl, requestOptions)
             .do(data => console.log('All: ' + JSON.stringify(data)))
             .catch(this.handleError);
     }

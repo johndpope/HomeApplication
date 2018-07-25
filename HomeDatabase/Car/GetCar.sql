6e9cf66ea81c10d562de672fdc37526e5115fb45
Alec Malcolm <alec.malcolm@camis.com>
2018-07-25 09:01:01 -0400

    stored procedure fix

--- HomeDatabase/Car/GetCar.sql
+++ HomeDatabase/Car/GetCar.sql
@@ -1,5 +1,5 @@
 ﻿CREATE PROCEDURE [dbo].[GetCar]
 	@id int
 AS
-	SELECT [carId], [make], [model], [year], [licensePlate] [enabled] FROM Car WHERE carId = @id;
+	SELECT [carId], [make], [model], [year], [licensePlate], [enabled] FROM Car WHERE carId = @id;
 RETURN 0
